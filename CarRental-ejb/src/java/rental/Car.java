bc7ac448b1e4dada6adba86319a73886ee5d1cba
Stefan Pante <s0203809@tienen.cs.kotnet.kuleuven.be>
2013-11-05 12:18:13 +0100

    Initial commit

---
+++ CarRental-ejb/src/java/rental/Car.java
@@ -0,0 +1,86 @@
+package rental;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Car {
+
+    @Id
+    private int id;
+    
+    @OneToOne
+    private CarType type;
+    
+    @OneToMany
+    private Set<Reservation> reservations;
+
+    
+
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+    
+    public Car() {
+    }
+    
+    public Car(int uid, CarType type) {
+    	this.id = uid;
+        this.type = type;
+        this.reservations = new HashSet<Reservation>();
+    }
+
+    /******
+     * ID *
+     ******/
+    
+    public int getId() {
+    	return id;
+    }
+    
+    public void setId(int id){
+        this.id = id;
+    }
+    
+    /************
+     * CAR TYPE *
+     ************/
+    
+    public CarType getType() {
+        return type;
+    }
+
+    /****************
+     * RESERVATIONS *
+     ****************/
+
+    public boolean isAvailable(Date start, Date end) {
+        if(!start.before(end))
+            throw new IllegalArgumentException("Illegal given period");
+
+        for(Reservation reservation : reservations) {
+            if(reservation.getEndDate().before(start) || reservation.getStartDate().after(end))
+                continue;
+            return false;
+        }
+        return true;
+    }
+    
+    public void addReservation(Reservation res) {
+        reservations.add(res);
+    }
+    
+    public void removeReservation(Reservation reservation) {
+        // equals-method for Reservation is required!
+        reservations.remove(reservation);
+    }
+
+    public Set<Reservation> getReservations() {
+        return reservations;
+    }
+}
---
+++ CarRental-lib/src/rental/Reservation.java
@@ -0,0 +1,49 @@
+package rental;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Reservation extends Quote {
+
+    private int carId;
+    
+    @Id
+    private Long id;
+    
+    /***************
+     * CONSTRUCTOR *
+     ***************/
+
+    public Reservation(Quote quote, int carId) {
+    	super(quote.getCarRenter(), quote.getStartDate(), quote.getEndDate(), 
+    		quote.getRentalCompany(), quote.getCarType(), quote.getRentalPrice());
+        this.carId = carId;
+    }
+    
+    /******
+     * ID *
+     ******/
+    
+    public int getCarId() {
+    	return carId;
+    }
+    
+    /*************
+     * TO STRING *
+     *************/
+    
+    @Override
+    public String toString() {
+        return String.format("Reservation for %s from %s to %s at %s\nCar type: %s\tCar: %s\nTotal price: %.2f", 
+                getCarRenter(), getStartDate(), getEndDate(), getRentalCompany(), getCarType(), getCarId(), getRentalPrice());
+    }	
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
